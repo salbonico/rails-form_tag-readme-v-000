f0cf2063d2c207e6380cd2cdc434861006c3f699
Steve Albonico <salbonico@gmail.com>
2019-02-20 22:35:02 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,9 +14,9 @@ describe 'new post' do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
- 
+
     click_on 'Submit Post'
- 
+
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
@@ -24,5 +24,5 @@ describe 'new post' do
 
 
 
-    
+
 end
